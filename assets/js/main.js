2b21bb7792c9f5ea4812f46de92bee4c09a8dc18
Rubankumar Moorthy <r_moor02@uni-muenster.de>
2022-07-27 00:39:41 +0200

    disable map

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,21 +263,7 @@
   });
 
 
-// Create the map
-  new Map({
-    target: 'map',
-    layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        })
-      })
-    ],
-    view: new View({
-      center: [0, 0],
-      zoom: 2
-    })
-  });
+
 
 
 
